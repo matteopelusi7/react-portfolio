f0440f27384fae51c2656c4c7bc3310fa8f4deb2
matteopelusi7 <pelusimatteo02@gmail.com>
2023-03-01 15:55:41 +0100

    completed portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,8 +3,13 @@ import spotify from '../img/spotify.PNG'
 import boolzap from '../img/boolzap.PNG'
 import playstation from '../img/playstation.png'
 import nicecream from '../img/nicecream.png'
+import discord from '../img/discord.png'
+import dropbox from '../img/dropbox.png'
+import proj from '../img/proj.png'
+import wikidrink from '../img/wikidrink.png'
 import { BsArrowRightShort } from "react-icons/bs";
 import { FaHtml5, FaCss3Alt, FaVuejs, FaReact } from "react-icons/fa";
+import { SiJavascript } from "react-icons/si";
 
 const Portfolio = () => {
   return (
@@ -14,40 +19,6 @@ const Portfolio = () => {
             <h5>Alcuni dei progetti che ho realizzato</h5>
         </div>
         <div className='card-container'>
-            <div className='card'>
-                <img src={spotify} alt="" />
-                <div className='overlay'>
-                    <p className='title'>Spotify</p>
-                    <p className='tec'>Tecnologie Utilizzate:</p>
-                    <div className='icon'>
-                        <FaHtml5 className='html' />
-                        <FaCss3Alt className='css' />
-                    </div>
-                    <button className='button'>
-                        <a href="https://spotify-web-1.netlify.app/">
-                            Visita il sito
-                            <BsArrowRightShort />
-                        </a>
-                    </button>
-                </div>
-            </div>
-            <div className='card'>
-                <img src={boolzap} alt="" />
-                <div className='overlay'>
-                    <p className='title'>Whatsapp</p>
-                    <p className='tec'>Tecnologie Utilizzate:</p>
-                    <div className='icon'>
-                        <FaVuejs className='vue' />
-                        <FaCss3Alt className='css' />
-                    </div>
-                    <button className='button'>
-                        <a href="https://boolzapp-1.netlify.app/">
-                            Visita il sito
-                            <BsArrowRightShort />
-                        </a>
-                    </button>
-                </div>
-            </div>
             <div className='card'>
                 <img src={playstation} alt="" />
                 <div className='overlay'>
@@ -66,6 +37,41 @@ const Portfolio = () => {
                 </div>
             </div>
             <div className='card'>
+                <img src={boolzap} alt="" />
+                <div className='overlay'>
+                    <p className='title'>Whatsapp</p>
+                    <p className='tec'>Tecnologie Utilizzate:</p>
+                    <div className='icon'>
+                        <FaVuejs className='vue' />
+                        <FaCss3Alt className='css' />
+                        <SiJavascript className='javascript' />
+                    </div>
+                    <button className='button'>
+                        <a href="https://boolzapp-1.netlify.app/">
+                            Visita il sito
+                            <BsArrowRightShort />
+                        </a>
+                    </button>
+                </div>
+            </div>
+            <div className='card'>
+                <img src={spotify} alt="" />
+                <div className='overlay'>
+                    <p className='title'>Spotify</p>
+                    <p className='tec'>Tecnologie Utilizzate:</p>
+                    <div className='icon'>
+                        <FaHtml5 className='html' />
+                        <FaCss3Alt className='css' />
+                    </div>
+                    <button className='button'>
+                        <a href="https://spotify-web-1.netlify.app/">
+                            Visita il sito
+                            <BsArrowRightShort />
+                        </a>
+                    </button>
+                </div>
+            </div>
+            <div className='card'>
                 <img src={nicecream} alt="" />
                 <div className='overlay'>
                     <p className='title'>Nice Cream</p>
@@ -73,9 +79,80 @@ const Portfolio = () => {
                     <div className='icon'>
                         <FaReact className='react' />
                         <FaCss3Alt className='css' />
+                        <SiJavascript className='javascript' />
                     </div>
                     <button className='button'>
                         <a href="https://react-nice-cream.netlify.app/">
+                            Visita il sito
+                            <BsArrowRightShort />
+                        </a>
+                    </button>
+                </div>
+            </div>
+            <div className='card'>
+                <img src={discord} alt="" />
+                <div className='overlay'>
+                    <p className='title'>Discord</p>
+                    <p className='tec'>Tecnologie Utilizzate:</p>
+                    <div className='icon'>
+                        <FaHtml5 className='html' />
+                        <FaCss3Alt className='css' />
+                    </div>
+                    <button className='button'>
+                        <a href="https://discord-1.netlify.app/">
+                            Visita il sito
+                            <BsArrowRightShort />
+                        </a>
+                    </button>
+                </div>
+            </div>
+            <div className='card'>
+                <img src={dropbox} alt="" />
+                <div className='overlay'>
+                    <p className='title'>Dropbox</p>
+                    <p className='tec'>Tecnologie Utilizzate:</p>
+                    <div className='icon'>
+                        <FaHtml5 className='html' />
+                        <FaCss3Alt className='css' />
+                    </div>
+                    <button className='button'>
+                        <a href="https://dropbox-1.netlify.app/">
+                            Visita il sito
+                            <BsArrowRightShort />
+                        </a>
+                    </button>
+                </div>
+            </div>
+            <div className='card'>
+                <img src={proj} alt="" />
+                <div className='overlay'>
+                    <p className='title'>Progetto Front-end</p>
+                    <p className='tec'>Tecnologie Utilizzate:</p>
+                    <div className='icon'>
+                        <FaVuejs className='vue' />
+                        <FaCss3Alt className='css' />
+                        <SiJavascript className='javascript' />
+                    </div>
+                    <button className='button'>
+                        <a href="https://proj-final.netlify.app/">
+                            Visita il sito
+                            <BsArrowRightShort />
+                        </a>
+                    </button>
+                </div>
+            </div>
+            <div className='card'>
+                <img src={wikidrink} alt="" />
+                <div className='overlay'>
+                    <p className='title'>Wiki Drink</p>
+                    <p className='tec'>Tecnologie Utilizzate:</p>
+                    <div className='icon'>
+                        <FaReact className='react' />
+                        <FaCss3Alt className='css' />
+                        <SiJavascript className='javascript' />
+                    </div>
+                    <button className='button'>
+                        <a href="https://wiki-drink.netlify.app/">
                             Visita il sito
                             <BsArrowRightShort />
                         </a>
