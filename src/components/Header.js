327c40cbde92719b761434e2ea4d37d20664c52a
matteopelusi7 <pelusimatteo02@gmail.com>
2023-03-02 11:28:46 +0100

    add hamburger-menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from "../img/logo.png";
+import { FaBars, FaHamburger } from "react-icons/fa";
 
 const Header = () => {
+
+  const [show, setShow] = useState(false)
+
   return (
     <div className='header container'>
         <nav className='navbar'>
             <div className='img-container'>
                 <img src={logo} alt="" className='img' />
+            </div>
+            <div className='pr'>
+              <button className='hamburger-menu' onClick={() => setShow(!show)}>
+                <FaBars />
+              </button>
             </div>
             <ul className='hamburger-list'>
                 <li>Home</li>
@@ -16,6 +25,13 @@ const Header = () => {
                 <li>Contatti</li>
             </ul>
         </nav>
+            <ul className={show ? '' : 'hamburger-list-show'}>
+                <li>Home</li>
+                <li>About</li>
+                <li>portfolio</li>
+                <li>Skills</li>
+                <li>Contatti</li>
+            </ul>
     </div>
   )
 }
