9c06187fc0706ad38b1d73e3a26c9091153d4e26
matteopelusi7 <pelusimatteo02@gmail.com>
2024-02-10 10:52:46 +0100

    add new info

--- src/components/About.js
+++ src/components/About.js
@@ -1,51 +1,90 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div id='about' className='container about'>
-        <div className='home home-about'>
-            <h2>About Me</h2>
-            <h5>Le mie esperienze</h5>
+    <div id="about" className="container about">
+      <div className="home home-about">
+        <h2>About Me</h2>
+        <h5>Le mie esperienze</h5>
+      </div>
+      <div className="container-experience">
+        <div className="experience">
+          <div className="info-experience">
+            <img
+              src="https://sial.school/wp-content/uploads/2022/03/Progetto-senza-titolo-30.png"
+              alt=""
+            />
+          </div>
+          <div className="description-experience">
+            <p className="name">Liceo scientifico</p>
+            <p className="name-experience">Diploma Liceo Scientifico</p>
+            <p className="date">set 2016 - giu 2021</p>
+            <p className="description">Votazione: 78/100</p>
+          </div>
         </div>
-        <div className='container-experience'>
-            <div className='experience'>
-                <div className='info-experience'>
-                    <img src="https://sial.school/wp-content/uploads/2022/03/Progetto-senza-titolo-30.png" alt="" />
-                </div>
-                <div className='description-experience'>
-                    <p className='name'>Liceo scientifico</p>
-                    <p className='name-experience'>Diploma Liceo Scientifico</p>
-                    <p className='date'>set 2016 - giu 2021</p>
-                    <p className='description'>Votazione: 78/100</p>
-                </div>
-            </div>
-            <div className='bar'></div>
-            <div className='experience'>
-                <div className='info-experience'>
-                    <img src="https://media.licdn.com/dms/image/C4D0BAQErbNlWeMNGDQ/company-logo_100_100/0/1600963347198?e=1685577600&v=beta&t=hFy4BJVR-59b-lJxS9Z5B3XnQkRQIbfD9Wmn0GeYlmc" alt="" />
-                </div>
-                <div className='description-experience'>
-                    <p className='name'>Boolean</p>
-                    <p className='name-experience'>Master, Full Stack Web Developer, Jr Full Stack Web Developer Trainee</p>
-                    <p className='date'>nov 2021 - giu 2022</p>
-                    <p className='description'>Durante i 6 mesi di corso ho avuto modo di fare molta pratica con diversi progetti ispirati alle più famose web applications come Whatsapp Web, Spotify Web, Netflix, Playstatio e molte altre. Ho imparato a utilizzare efficacemente tecnologie come HTML5, CSS3, JavaScript, SASS, VueJS, JQuery, PHP, MySQL e Laravel.</p>
-                </div>
-            </div>
-            <div className='bar'></div>
-            <div className='experience'>
-                <div className='info-experience'>
-                    <img src="https://sial.school/wp-content/uploads/2022/03/Progetto-senza-titolo-30.png" alt="" />
-                </div>
-                <div className='description-experience'>
-                    <p className='name'>Corso React</p>
-                    <p className='name-experience'>React.js e Redux: Teoria, Hooks + 11 Progetti</p>
-                    <p className='date'>dic 2022 - gen 2023</p>
-                    <p className='description'>Apprendimento di React.js e Redux tramite teoria, Hooks e 11 progetti svolti</p>
-                </div>
-            </div>
+        <div className="bar"></div>
+        <div className="experience">
+          <div className="info-experience">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOYEAsfh4FaGWUZiN8sMvIsUW4r-XJXWhx65_vK7zzSw&s"
+              alt=""
+            />
+          </div>
+          <div className="description-experience">
+            <p className="name">Boolean</p>
+            <p className="name-experience">
+              Master, Full Stack Web Developer, Jr Full Stack Web Developer
+              Trainee
+            </p>
+            <p className="date">nov 2021 - giu 2022</p>
+            <p className="description">
+              Durante i 6 mesi di corso ho avuto modo di fare molta pratica con
+              diversi progetti ispirati alle più famose web applications come
+              Whatsapp Web, Spotify Web, Netflix, Playstatio e molte altre. Ho
+              imparato a utilizzare efficacemente tecnologie come HTML5, CSS3,
+              JavaScript, SASS, VueJS, JQuery, PHP, MySQL e Laravel.
+            </p>
+          </div>
         </div>
-    </div>
-  )
-}
+        <div className="bar"></div>
+        <div className="experience">
+          <div className="info-experience">
+            <img
+              src="https://sial.school/wp-content/uploads/2022/03/Progetto-senza-titolo-30.png"
+              alt=""
+            />
+          </div>
+          <div className="description-experience">
+            <p className="name">Corso React</p>
+            <p className="name-experience">
+              React.js e Redux: Teoria, Hooks + 11 Progetti
+            </p>
+            <p className="date">dic 2022 - gen 2023</p>
+            <p className="description">
+              Apprendimento di React.js e Redux tramite teoria, Hooks e 11
+              progetti svolti
+            </p>
+          </div>
+        </div>
 
-export default About
+        <div className="bar"></div>
+        <div className="experience">
+          <div className="info-experience">
+            <img
+              src="https://media.licdn.com/dms/image/C560BAQHAARQiWAkmGQ/company-logo_100_100/0/1631357767874?e=1715817600&v=beta&t=vsOpda-2Pssbdu4R_9yzHzLZZXMGF8idUUk_NlJZa7Q"
+              alt=""
+            />
+          </div>
+          <div className="description-experience">
+            <p className="name">ViVieb</p>
+            <p className="name-experience">Frontend Developer</p>
+            <p className="date">apr 2023 - presente</p>
+            <p className="description">Pineto, Abruzzo, Italia</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,56 +1,104 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
   return (
-    <div id='skills' className='container'>
-        <div className='home home-about portfolio container'>
-            <h2>Skills</h2>
-            <h5>Le mie conoscenze tecniche</h5>
-        </div>
-        <div className='container-skills'>
-            <ul className='frontend'>
-                <div className='list'>
-                    <li>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/260px-HTML5_logo_and_wordmark.svg.png" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/100px-CSS3_logo_and_wordmark.svg.png" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/180px-Unofficial_JavaScript_logo_2.svg.png" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/100px-React-icon.svg.png" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://cdn.worldvectorlogo.com/logos/redux.svg" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/100px-Vue.js_Logo_2.svg.png" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/100px-Bootstrap_logo.svg.png" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://sass-lang.com/assets/img/styleguide/color-1c4aab2b.png" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/260px-PHP-logo.svg.png" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/100px-Laravel.svg.png" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://www.vectorlogo.zone/logos/mysql/mysql-official.svg" alt="" />
-                    </li>
-                    <li>
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/9/95/PhpMyAdmin_logo.png" alt="" />
-                    </li>
-                </div>
-            </ul>
-        </div>
+    <div id="skills" className="container">
+      <div className="home home-about portfolio container">
+        <h2>Skills</h2>
+        <h5>Le mie conoscenze tecniche</h5>
+      </div>
+      <div className="container-skills">
+        <ul className="frontend">
+          <div className="list">
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/260px-HTML5_logo_and_wordmark.svg.png"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/100px-CSS3_logo_and_wordmark.svg.png"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/180px-Unofficial_JavaScript_logo_2.svg.png"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/2048px-Typescript_logo_2020.svg.png"
+                alt=""
+              ></img>
+            </li>
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/100px-React-icon.svg.png"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://cdn.worldvectorlogo.com/logos/redux.svg"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://static-00.iconduck.com/assets.00/next-js-icon-2048x2048-5dqjgeku.png"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/100px-Vue.js_Logo_2.svg.png"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Bootstrap_logo.svg/100px-Bootstrap_logo.svg.png"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://sass-lang.com/assets/img/styleguide/color-1c4aab2b.png"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/PHP-logo.svg/260px-PHP-logo.svg.png"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Laravel.svg/100px-Laravel.svg.png"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://www.vectorlogo.zone/logos/mysql/mysql-official.svg"
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/9/95/PhpMyAdmin_logo.png"
+                alt=""
+              />
+            </li>
+          </div>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
